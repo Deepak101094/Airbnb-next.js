d0fad9521b177d544342a5e7b03b825e4a76c5af
Deepak/101094 <deepakpandey101094@gmail.com>
2023-12-28 12:00:50 +0530

    complete authentication

---
+++ app/hooks/useRentModal.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import { create } from "zustand";
+
+interface RentModalProps {
+	isOpen: boolean;
+	onOpen: () => void;
+	onClose: () => void;
+}
+
+const useRentModal = create<RentModalProps>((set) => {
+	return {
+		isOpen: false,
+		onOpen: () => set({ isOpen: true }),
+		onClose: () => set({ isOpen: false }),
+	};
+});
+
+export default useRentModal;
